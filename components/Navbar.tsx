928fe6730cc1fd1e08870d4727fb983ceae95c7e
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-07 08:53:12 -0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,15 @@
 export default function Navbar() {
   return (
-    <header className="navbar premiumNavbar">
-      <a href="#inicio" className="brand premiumBrand">
-        <span className="brandMark">Z</span>
-        <span>
+    <header className="topNavbar">
+      <a href="#inicio" className="zusasaLogo">
+        <span className="logoIcon">Z</span>
+        <span className="logoText">
           <small>TRANSPORTES</small>
           <strong>ZUSASA</strong>
         </span>
       </a>
 
-      <nav className="navlinks premiumNavlinks">
+      <nav className="topNavlinks">
         <a href="#inicio">Inicio</a>
         <a href="#nosotros">Nosotros</a>
         <a href="#servicios">Servicios</a>
@@ -17,7 +17,7 @@ export default function Navbar() {
         <a href="#contacto">Contacto</a>
       </nav>
 
-      <a className="navWhatsapp premiumButton" href="https://wa.me/523148721897">
+      <a className="quoteButton" href="https://wa.me/523148721897">
         Cotizar ahora
       </a>
     </header>
