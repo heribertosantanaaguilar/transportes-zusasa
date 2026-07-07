45cef0e35788a306468d08a24ad2b00c6c687f0d
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-07 06:21:23 -0600

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h2>Transportes ZUSASA</h2>
+        <p>Moviendo la logística que impulsa tu negocio.</p>
+      </div>
+
+      <div>
+        <p>Manzanillo, Colima, México</p>
+        <p>transportes.zusasa@gmail.com</p>
+      </div>
+    </footer>
+  );
+}
