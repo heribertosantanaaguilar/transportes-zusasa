13277f6a89478018491e263d89b694e22b0c661a
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-07 06:08:59 -0600

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">TRANSPORTES <strong>ZUSASA</strong></div>
+      <div className="navlinks">
+        <a href="#inicio">Inicio</a>
+        <a href="#nosotros">Nosotros</a>
+        <a href="#servicios">Servicios</a>
+        <a href="#contacto">Contacto</a>
+      </div>
+      <a className="whatsapp" href="https://wa.me/523148721897">WhatsApp</a>
+    </nav>
+  );
+}
