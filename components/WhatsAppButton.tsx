4367108b2a647c57ec6052d0b86effc1a2f89d3c
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-07 06:28:31 -0600

    Create WhatsAppButton.tsx

---
+++ components/WhatsAppButton.tsx
@@ -0,0 +1,13 @@
+export default function WhatsAppButton() {
+  return (
+    <a
+      className="floatingWhatsApp"
+      href="https://wa.me/523148721897"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Contactar por WhatsApp"
+    >
+      WhatsApp
+    </a>
+  );
+}
