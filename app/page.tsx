abe3c2bce798ba0b312eed6fce567e068a011697
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-06 21:31:00 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Transportes ZUSASA</h1>
+      <p>Movimiento local de contenedores en el Puerto de Manzanillo.</p>
+    </main>
+  );
+}
