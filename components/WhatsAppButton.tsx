4b6d721a3d4ff059da0b40df6f8d324c17d0a291
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-08 17:18:33 -0600

    Update WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -2,10 +2,10 @@ export default function WhatsAppButton() {
   return (
     <a
       className="floatingWhatsApp"
-      href="https://wa.me/523148721897"
+      href="https://wa.me/522224556651?text=Hola%2C%20quiero%20solicitar%20una%20cotizaci%C3%B3n%20para%20un%20movimiento%20local%20de%20carga%20en%20Manzanillo."
       target="_blank"
       rel="noopener noreferrer"
-      aria-label="Contactar por WhatsApp"
+      aria-label="Contactar por WhatsApp con Gerencia General"
     >
       WhatsApp
     </a>
