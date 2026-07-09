2dabef3904cde48fc0f7a3dec81ff93744fd04e4
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-09 09:59:14 -0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,22 +27,23 @@ export default function Footer() {
 
       <div>
         <h3>Navegación</h3>
-        <a href="#inicio">Inicio</a>
-        <a href="#nosotros">Nosotros</a>
-        <a href="#servicios">Servicios</a>
-        <a href="#sectores">Sectores</a>
-        <a href="#contacto">Contacto</a>
+        <a href="/#inicio">Inicio</a>
+        <a href="/#nosotros">Nosotros</a>
+        <a href="/#servicios">Servicios</a>
+        <a href="/#sectores">Sectores</a>
+        <a href="/#contacto">Contacto</a>
+        <a href="/cotizacion">Solicitar cotización</a>
         <a href="/aviso-de-privacidad">Aviso de privacidad</a>
       </div>
 
       <div>
         <h3>Áreas de contacto</h3>
 
-        <a href="#gerencia-general">Gerencia General</a>
-        <a href="#administracion-finanzas">
+        <a href="/#gerencia-general">Gerencia General</a>
+        <a href="/#administracion-finanzas">
           Dirección de Administración y Finanzas
         </a>
-        <a href="#coordinacion-logistica">Coordinación de Logística</a>
+        <a href="/#coordinacion-logistica">Coordinación de Logística</a>
 
         <p className="footerEmail">
           <strong>Correo:</strong>
