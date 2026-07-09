76123318d9c66c5290183a718038c66925f1abf4
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-09 09:56:51 -0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <header className="topNavbar">
-      <a href="#inicio" className="zusasaLogo">
+      <a href="/#inicio" className="zusasaLogo">
         <img
           className="navbarLogoImage"
           src="/images/logo-zusasa.png"
@@ -15,19 +15,14 @@ export default function Navbar() {
       </a>
 
       <nav className="topNavlinks">
-        <a href="#inicio">Inicio</a>
-        <a href="#nosotros">Nosotros</a>
-        <a href="#servicios">Servicios</a>
-        <a href="#sectores">Sectores</a>
-        <a href="#contacto">Contacto</a>
+        <a href="/#inicio">Inicio</a>
+        <a href="/#nosotros">Nosotros</a>
+        <a href="/#servicios">Servicios</a>
+        <a href="/#sectores">Sectores</a>
+        <a href="/#contacto">Contacto</a>
       </nav>
 
-      <a
-        className="quoteButton"
-        href="https://wa.me/522224556651?text=Hola%2C%20quiero%20solicitar%20una%20cotizaci%C3%B3n%20para%20un%20movimiento%20local%20de%20carga%20en%20Manzanillo."
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a className="quoteButton" href="/cotizacion">
         Cotizar ahora
       </a>
     </header>
