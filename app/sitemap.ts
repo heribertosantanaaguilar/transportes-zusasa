655e6a17b93969484afa7a3b0077f00598271e5b
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-10 15:46:23 -0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,6 +15,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: "https://transporteszusasa.com/transporte-de-contenedores-manzanillo",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: "https://transporteszusasa.com/resguardo-de-contenedores-manzanillo",
       lastModified: new Date(),
       changeFrequency: "monthly",
