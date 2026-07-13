4b1749753dfc4a58bb95064b88e15bc5690974c1
heribertosantanaaguilar <heriberto.santana25@gmail.com>
2026-07-13 09:49:55 -0600

    Update WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -2,10 +2,10 @@ export default function WhatsAppButton() {
   return (
     <a
       className="floatingWhatsApp"
-      href="https://wa.me/522224556651?text=Hola%2C%20quiero%20solicitar%20una%20cotizaci%C3%B3n%20para%20un%20movimiento%20local%20de%20carga%20en%20Manzanillo."
+      href="https://wa.me/522224556651?text=Hola%2C%20quiero%20solicitar%20informaci%C3%B3n%20y%2Fo%20cotizaci%C3%B3n%20con%20Transportes%20ZUSASA%20para%20un%20servicio%20en%20Manzanillo.%0A%0AServicio%20que%20necesito%3A%0A-%20Movimiento%20local%20de%20contenedores%3A%0A-%20Resguardo%20en%20patio%20de%20maniobras%3A%0A-%20Apoyo%20operativo%20para%20contenedores%3A%0A-%20Seguimiento%20de%20operaci%C3%B3n%3A%0A-%20Otro%3A%0A%0ADatos%20del%20servicio%3A%0A-%20Empresa%3A%0A-%20Nombre%20de%20contacto%3A%0A-%20Tel%C3%A9fono%3A%0A-%20Tipo%20de%20contenedor%3A%0A-%20Origen%3A%0A-%20Destino%3A%0A-%20Fecha%20estimada%3A%0A-%20Comentarios%3A"
       target="_blank"
       rel="noopener noreferrer"
-      aria-label="Contactar por WhatsApp con Gerencia General"
+      aria-label="Contactar por WhatsApp con Transportes ZUSASA"
     >
       WhatsApp
     </a>
